51b5e70e3bf43c0a28a8c554fd42699d50c0a468
kquach229 <kquach229@gmail.com>
2025-07-25 17:06:57 -0400

    resume tooltip

--- app/interview/[id]/page.tsx
+++ app/interview/[id]/page.tsx
@@ -2,6 +2,12 @@ import { prisma } from '@/lib/prisma';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import MockInterviewPage from '@/components/MockInterview';
 import PracticeQuestionsList from '@/components/PracticeQuestionsList';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+import { InfoIcon } from 'lucide-react';
 
 interface InterviewPageProps {
   params: { id: string };
@@ -19,10 +25,33 @@ export default async function InterviewPage({ params }: InterviewPageProps) {
     },
   });
 
+  const interview = await prisma.interview.findUnique({
+    where: {
+      id: id,
+    },
+    include: {
+      resume: true,
+    },
+  });
+
   return (
     <div className='max-w-3xl mx-auto p-4'>
-      <h1 className='text-2xl font-bold mb-4'>Preparation</h1>
-
+      <h1 className='text-4xl font-bold mb-4'>{`${interview?.jobTitle} Interview Questions`}</h1>
+      {interview?.resume?.fileName && (
+        <div className='text-right text-xs flex items-center gap-x-1'>
+          {interview.resume.fileName}
+          <Tooltip>
+            <TooltipTrigger>
+              <InfoIcon className='w-3' />
+            </TooltipTrigger>
+            <TooltipContent>
+              You provided a resume file for this position on{' '}
+              {`${new Date(interview.resume.createdAt).toLocaleDateString()}`}.
+              Some questions are tailored based on your provided resume
+            </TooltipContent>
+          </Tooltip>
+        </div>
+      )}
       <div className='mt-20 mb-20'>
         <Tabs defaultValue='practice-questions'>
           <TabsList className='w-full'>

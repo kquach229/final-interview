d138fc74bd95fb804b37b32b54918c1003017acf
kquach229 <kquach229@gmail.com>
2025-08-18 16:46:34 -0400

    sidebar width

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,25 +1,25 @@
-import React from "react";
+import React from 'react';
 import {
   Sidebar,
   SidebarContent,
   SidebarGroup,
   SidebarHeader,
-} from "./ui/sidebar";
-import Image from "next/image";
-import { UserButton } from "@clerk/nextjs";
-import { currentUser } from "@clerk/nextjs/server";
-import SignupButtonComponent from "./SignupButtonComponent";
-import { Button } from "./ui/button";
+} from './ui/sidebar';
+import Image from 'next/image';
+import { UserButton } from '@clerk/nextjs';
+import { currentUser } from '@clerk/nextjs/server';
+import SignupButtonComponent from './SignupButtonComponent';
+import { Button } from './ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { ChevronDown, Plus } from "lucide-react";
-import Link from "next/link";
-import { prisma } from "@/lib/prisma";
-import SidebarInterviewSelection from "./SidebarInterviewSelection";
+} from '@/components/ui/dropdown-menu';
+import { ChevronDown, Plus } from 'lucide-react';
+import Link from 'next/link';
+import { prisma } from '@/lib/prisma';
+import SidebarInterviewSelection from './SidebarInterviewSelection';
 
 const AppSidebar = async ({}) => {
   const loggedInUser = await currentUser();
@@ -31,26 +31,26 @@ const AppSidebar = async ({}) => {
     : [];
 
   return (
-    <Sidebar className="w-[200px]">
+    <Sidebar>
       <SidebarHeader>
-        <Link href="/">
+        <Link href='/'>
           <Image
-            src="/final_interview_logo.svg"
-            alt="final interview logo"
+            src='/final_interview_logo.svg'
+            alt='final interview logo'
             height={400}
             width={400}
           />
         </Link>
       </SidebarHeader>
-      <SidebarContent className="flex flex-col justify-between">
+      <SidebarContent className='flex flex-col justify-between'>
         {loggedInUser && (
           <div>
             <SidebarGroup>
               <DropdownMenu>
-                <DropdownMenuTrigger className="text-left">
-                  <Button className="w-full flex justify-between">
-                    <div className="inline-flex items-center gap-2">
-                      <Plus className="text-left" />
+                <DropdownMenuTrigger className='text-left'>
+                  <Button className='w-full flex justify-between'>
+                    <div className='inline-flex items-center gap-2'>
+                      <Plus className='text-left' />
                       Create
                     </div>
 
@@ -59,20 +59,20 @@ const AppSidebar = async ({}) => {
                 </DropdownMenuTrigger>
                 <DropdownMenuContent>
                   <DropdownMenuItem>
-                    <Link href="/prep/role-form">Create Interview Prep</Link>
+                    <Link href='/prep/role-form'>Create Interview Prep</Link>
                   </DropdownMenuItem>
                 </DropdownMenuContent>
               </DropdownMenu>
             </SidebarGroup>
-            <SidebarGroup className="space-y-2 mt-12">
+            <SidebarGroup className='space-y-2 mt-12'>
               <SidebarInterviewSelection interviews={interviews} />
-            </SidebarGroup>{" "}
+            </SidebarGroup>{' '}
           </div>
         )}
 
         <SidebarGroup>
           {loggedInUser ? (
-            <div className="flex items-center gap-2">
+            <div className='flex items-center gap-2'>
               <UserButton />
               <span>{loggedInUser?.firstName}</span>
             </div>

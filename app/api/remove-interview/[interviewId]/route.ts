54090f47303aa114bcbddf00bfc638cb62a0849b
kquach229 <kquach229@gmail.com>
2025-08-22 23:48:10 -0400

    fix invalid param

--- app/api/remove-interview/[interviewId]/route.ts
+++ app/api/remove-interview/[interviewId]/route.ts
@@ -2,7 +2,10 @@ import { checkUser } from '@/lib/checkUser';
 import { prisma } from '@/lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function DELETE({ params }: { params: { interviewId: string } }) {
+export async function DELETE(
+  _req: NextRequest,
+  { params }: { params: { interviewId: string } }
+) {
   try {
     const user = await checkUser();
     const { interviewId } = await params;

81ee219846de49351fb23e9f70350cbe8fac3437
kquach229 <kquach229@gmail.com>
2025-08-22 23:54:19 -0400

    ts

--- app/api/remove-interview/[interviewId]/route.ts
+++ app/api/remove-interview/[interviewId]/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function DELETE(
   _req: NextRequest,
-  { params }: { params: { interviewId: string } }
+  { params }: { params: Record<string, string> }
 ) {
   try {
     const user = await checkUser();
